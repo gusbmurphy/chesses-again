5294cacf4a149a6adad3fbde9ca5d746d23a8d23
Augustus Murphy <gusbmurphy@gmail.com>
2025-07-03 23:12:40 -0500

    Move all tests from `RuleEngineSpec` to `MoveEvaluationRuleSuiteSpec`

---
+++ engine/src/test/groovy/fun/gusmurphy/chesses/engine/MoveEvaluationRuleSuiteSpec.groovy
@@ -0,0 +1,62 @@
+package fun.gusmurphy.chesses.engine
+
+import fun.gusmurphy.chesses.engine.doubles.IllegalAlwaysEvaluationRule
+import fun.gusmurphy.chesses.engine.doubles.LegalAlwaysEvaluationRule
+import spock.lang.Specification
+
+import static fun.gusmurphy.chesses.engine.MoveLegality.ILLEGAL
+import static fun.gusmurphy.chesses.engine.MoveLegality.LEGAL
+
+class MoveEvaluationRuleSuiteSpec extends Specification {
+
+    def "with no rules, a move is legal"() {
+        given:
+        MoveEvaluationRuleSuite ruleSuite = new MoveEvaluationRuleSuite()
+
+        expect:
+        ruleSuite.evaluate() == LEGAL
+    }
+
+    def "with just one move evaluation rule, a move is legal or illegal based on just that one"() {
+        given:
+        MoveEvaluationRuleSuite ruleSuite = new MoveEvaluationRuleSuite(rule)
+
+        when:
+        def result = ruleSuite.evaluate()
+
+        then:
+        result == expected
+
+        where:
+        rule                              | expected
+        new IllegalAlwaysEvaluationRule() | ILLEGAL
+        new LegalAlwaysEvaluationRule()   | LEGAL
+    }
+
+    def "with two move evaluation rules, a move is legal if both allow it"() {
+        given:
+        MoveEvaluationRule ruleOne = new LegalAlwaysEvaluationRule()
+        MoveEvaluationRule ruleTwo = new LegalAlwaysEvaluationRule()
+        MoveEvaluationRuleSuite ruleSuite = new MoveEvaluationRuleSuite(ruleOne, ruleTwo)
+
+        when:
+        def result = ruleSuite.evaluate()
+
+        then:
+        result == LEGAL
+    }
+
+    def "with two move evaluation rules, a move is illegal if one does not allow it"() {
+        given:
+        MoveEvaluationRule legal = new LegalAlwaysEvaluationRule()
+        MoveEvaluationRule illegal = new IllegalAlwaysEvaluationRule()
+        MoveEvaluationRuleSuite ruleSuite = new MoveEvaluationRuleSuite(legal, illegal)
+
+        when:
+        def result = ruleSuite.evaluate()
+
+        then:
+        result == ILLEGAL
+    }
+
+}
