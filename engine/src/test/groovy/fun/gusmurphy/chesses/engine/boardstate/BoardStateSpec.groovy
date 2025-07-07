0e02151f9379803ba73bca2fccdc3bb9a7827a17
Augustus Murphy <gusbmurphy@gmail.com>
2025-07-06 20:04:31 -0500

    Move some tests into more specific `PieceMovedEventSpec`

--- engine/src/test/groovy/fun/gusmurphy/chesses/engine/boardstate/BoardStateSpec.groovy
+++ engine/src/test/groovy/fun/gusmurphy/chesses/engine/boardstate/BoardStateSpec.groovy
@@ -1,13 +1,7 @@
 package fun.gusmurphy.chesses.engine.boardstate
 
-import fun.gusmurphy.chesses.engine.PlayerColor
-import fun.gusmurphy.chesses.engine.boardstate.events.BoardStateEvent
-import fun.gusmurphy.chesses.engine.boardstate.events.PieceMovedEvent
-import fun.gusmurphy.chesses.engine.piece.Piece
 import fun.gusmurphy.chesses.engine.piece.PieceId
 import spock.lang.Specification
-
-import static fun.gusmurphy.chesses.engine.Coordinates.*
 
 class BoardStateSpec extends Specification {
 
@@ -17,34 +11,6 @@ class BoardStateSpec extends Specification {
 
         when:
         boardState.pieceForId(new PieceId())
-
-        then:
-        thrown UnknownPieceException
-    }
-
-    def "a piece moved event will move the given piece on the board"() {
-        given:
-        Piece piece = new Piece(PlayerColor.WHITE)
-        BoardState board = new BoardStateBuilder()
-            .addPieceAt(piece, A7)
-            .build()
-        BoardStateEvent event = new PieceMovedEvent(piece.id(), B6)
-
-        when:
-        board.apply(event)
-
-        then:
-        board.positionForPieceId(piece.id()) == B6
-    }
-
-    def "a piece moved event for a piece not on the board throws an UnknownPieceException"() {
-        given:
-        BoardState board = new BoardStateBuilder().build()
-        Piece piece = new Piece(PlayerColor.WHITE)
-        BoardStateEvent event = new PieceMovedEvent(piece.id(), B6)
-
-        when:
-        board.apply(event)
 
         then:
         thrown UnknownPieceException
