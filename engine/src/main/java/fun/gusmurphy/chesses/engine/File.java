09e2406b084d4087040de34f4466264eab37301c
Augustus Murphy <gusbmurphy@gmail.com>
2025-07-19 19:21:29 -0500

    Add Javadocs to `File` and `Rank`

--- engine/src/main/java/fun/gusmurphy/chesses/engine/File.java
+++ engine/src/main/java/fun/gusmurphy/chesses/engine/File.java
@@ -1,5 +1,8 @@
 package fun.gusmurphy.chesses.engine;
 
+/**
+ * A vertical column on a board.
+ */
 public enum File {
     A, B, C, D, E, F, G, H
 }
--- engine/src/main/java/fun/gusmurphy/chesses/engine/Rank.java
+++ engine/src/main/java/fun/gusmurphy/chesses/engine/Rank.java
@@ -1,5 +1,8 @@
 package fun.gusmurphy.chesses.engine;
 
+/**
+ * A horizontal row on a board.
+ */
 public enum Rank {
     ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT
 }
