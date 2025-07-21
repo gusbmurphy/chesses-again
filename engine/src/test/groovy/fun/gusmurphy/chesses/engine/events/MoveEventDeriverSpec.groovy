6c2d051bce30ba723cf867d27296948659599776
Augustus Murphy <gusbmurphy@gmail.com>
2025-07-21 18:34:54 -0500

    Extract some reused test parts in `MoveEventDeriverSpec`

--- engine/src/test/groovy/fun/gusmurphy/chesses/engine/events/MoveEventDeriverSpec.groovy
+++ engine/src/test/groovy/fun/gusmurphy/chesses/engine/events/MoveEventDeriverSpec.groovy
@@ -12,12 +12,15 @@ import static fun.gusmurphy.chesses.engine.PlayerColor.*
 
 class MoveEventDeriverSpec extends Specification {
 
+    private final static Piece PIECE_A = new Piece(WHITE, ROOK)
+    private final static Piece PIECE_B = new Piece(BLACK, ROOK)
+
+    private final DerivesMoveEvents deriver = new MoveEventDeriver()
+
     def "moving a piece to another spot on the board does just that"() {
         given:
-        DerivesMoveEvents deriver = new MoveEventDeriver()
-        def piece = new Piece(WHITE, ROOK)
-        def board = new BoardStateBuilder().addPieceAt(piece, A3).build()
-        def move = new Move(piece.id(), A7)
+        def board = new BoardStateBuilder().addPieceAt(PIECE_A, A3).build()
+        def move = new Move(PIECE_A.id(), A7)
 
         when:
         def result = deriver.deriveEventsFrom(move, board)
@@ -26,19 +29,16 @@ class MoveEventDeriverSpec extends Specification {
         result.inOrder().size() == 1
         def event = result.inOrder()[0] as PieceMovedEvent
         event.newCoordinates() == A7
-        event.pieceId() == piece.id()
+        event.pieceId() == PIECE_A.id()
     }
 
     def "moving a piece to an occupied space removes the piece from that space, and then moves the piece"() {
         given:
-        DerivesMoveEvents deriver = new MoveEventDeriver()
-        def movingPiece = new Piece(WHITE, ROOK)
-        def takenPiece = new Piece(WHITE, ROOK)
         def board = new BoardStateBuilder()
-            .addPieceAt(movingPiece, A3)
-            .addPieceAt(takenPiece, D3)
+            .addPieceAt(PIECE_A, A3)
+            .addPieceAt(PIECE_B, D3)
             .build()
-        def move = new Move(movingPiece.id(), D3)
+        def move = new Move(PIECE_A.id(), D3)
 
         when:
         def result = deriver.deriveEventsFrom(move, board)
@@ -47,11 +47,11 @@ class MoveEventDeriverSpec extends Specification {
         result.inOrder().size() == 2
 
         def e1 = result.inOrder()[0] as PieceRemovedEvent
-        e1.pieceId() == takenPiece.id()
+        e1.pieceId() == PIECE_B.id()
 
         def e2 = result.inOrder()[1] as PieceMovedEvent
         e2.newCoordinates() == D3
-        e2.pieceId() == movingPiece.id()
+        e2.pieceId() == PIECE_A.id()
     }
 
 }
