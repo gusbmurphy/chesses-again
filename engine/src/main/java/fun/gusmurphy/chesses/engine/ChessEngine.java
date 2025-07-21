496a1a1cd5a548d9d060bff85a0d783b61d58c61
Augustus Murphy <gusbmurphy@gmail.com>
2025-07-21 10:09:11 -0500

    fixup! Create `ChessEngine`

--- engine/src/main/java/fun/gusmurphy/chesses/engine/ChessEngine.java
+++ engine/src/main/java/fun/gusmurphy/chesses/engine/ChessEngine.java
@@ -3,20 +3,19 @@ package fun.gusmurphy.chesses.engine;
 import fun.gusmurphy.chesses.engine.boardstate.BoardState;
 import fun.gusmurphy.chesses.engine.rules.MoveLegality;
 import fun.gusmurphy.chesses.engine.rules.MoveLegalityRule;
-import fun.gusmurphy.chesses.engine.rules.MoveLegalityRuleSuite;
 
 public class ChessEngine {
 
-    private final MoveLegalityRule ruleSuite;
+    private final MoveLegalityRule moveRule;
     private final BoardState boardState;
 
-    public ChessEngine(MoveLegalityRuleSuite ruleSuite, BoardState boardState) {
-        this.ruleSuite = ruleSuite;
+    public ChessEngine(MoveLegalityRule moveRule, BoardState boardState) {
+        this.moveRule = moveRule;
         this.boardState = boardState;
     }
 
     public MoveLegality checkLegalityOf(Move move) {
-        return ruleSuite.evaluate(boardState, move);
+        return moveRule.evaluate(boardState, move);
     }
 
 }
--- engine/src/test/groovy/fun/gusmurphy/chesses/engine/ChessEngineSpec.groovy
+++ engine/src/test/groovy/fun/gusmurphy/chesses/engine/ChessEngineSpec.groovy
@@ -3,23 +3,23 @@ package fun.gusmurphy.chesses.engine
 import fun.gusmurphy.chesses.engine.boardstate.BoardStateBuilder
 import fun.gusmurphy.chesses.engine.piece.PieceId
 import fun.gusmurphy.chesses.engine.rules.MoveLegality
-import fun.gusmurphy.chesses.engine.rules.MoveLegalityRuleSuite
+import fun.gusmurphy.chesses.engine.rules.MoveLegalityRule
 import spock.lang.Specification
 
 class ChessEngineSpec extends Specification {
 
-    def "the engine asks its rule suite for rule legality"() {
+    def "the engine asks its move rule for rule legality"() {
         given:
-        def ruleSuite = Mock(MoveLegalityRuleSuite)
+        def moveRule = Mock(MoveLegalityRule)
         def board = new BoardStateBuilder().build()
-        def engine = new ChessEngine(ruleSuite, board)
+        def engine = new ChessEngine(moveRule, board)
         def move = new Move(new PieceId(), Coordinates.A1)
 
         when:
         def result = engine.checkLegalityOf(move)
 
         then:
-        1 * ruleSuite.evaluate(board, move) >> MoveLegality.LEGAL
+        1 * moveRule.evaluate(board, move) >> MoveLegality.LEGAL
         result == MoveLegality.LEGAL
     }
 
