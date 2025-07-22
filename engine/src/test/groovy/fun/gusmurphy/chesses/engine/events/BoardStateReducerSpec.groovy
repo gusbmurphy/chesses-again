cc518ed1b10d81c7fc79170efe0e91de44c6d9b9
Augustus Murphy <gusbmurphy@gmail.com>
2025-07-21 19:38:17 -0500

    Move other event tests into `BoardStateReducerSpec`

--- engine/src/test/groovy/fun/gusmurphy/chesses/engine/events/BoardStateReducerSpec.groovy
+++ engine/src/test/groovy/fun/gusmurphy/chesses/engine/events/BoardStateReducerSpec.groovy
@@ -1,5 +1,6 @@
 package fun.gusmurphy.chesses.engine.events
 
+import fun.gusmurphy.chesses.engine.PlayerColor
 import fun.gusmurphy.chesses.engine.boardstate.BoardState
 import fun.gusmurphy.chesses.engine.boardstate.BoardStateBuilder
 import fun.gusmurphy.chesses.engine.boardstate.UnknownPieceException
@@ -40,6 +41,45 @@ class BoardStateReducerSpec extends Specification {
 
         then:
         thrown UnknownPieceException
+    }
+
+    def "a piece removed event removes a piece from the board"() {
+        given:
+        BoardStateEvent event = new PieceRemovedEvent(PIECE.id())
+
+        when:
+        def result = reducer.reduce(BOARD, event)
+        result.pieceOnBoardForId(PIECE.id())
+
+        then:
+        thrown UnknownPieceException
+    }
+
+    def "removing a piece that is not on the board throws an exception"() {
+        given:
+        BoardState emptyBoard = new BoardStateBuilder().build()
+        BoardStateEvent event = new PieceRemovedEvent(PIECE.id())
+
+        when:
+        emptyBoard.apply(event)
+
+        then:
+        thrown UnknownPieceException
+    }
+
+    def "a turn change event changes the current turn color"() {
+        given:
+        def boardWithTurnColor = new BoardStateBuilder().currentTurnColor(newColor.opposite()).build()
+        def turnChangeEvent = new TurnChangeEvent(newColor)
+
+        when:
+        def result = reducer.reduce(boardWithTurnColor, turnChangeEvent)
+
+        then:
+        result.currentTurnColor() == newColor
+
+        where:
+        newColor << [WHITE, BLACK]
     }
 
 }
