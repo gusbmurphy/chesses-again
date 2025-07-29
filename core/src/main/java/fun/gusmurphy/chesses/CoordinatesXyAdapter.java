382d5f613a1875faabbe524db8e68ceac9bbb36e
Augustus Murphy <gusbmurphy@gmail.com>
2025-07-29 18:28:24 -0500

    Bring over `CoordinatesXyAdapter`

---
+++ core/src/main/java/fun/gusmurphy/chesses/CoordinatesXyAdapter.java
@@ -0,0 +1,42 @@
+package fun.gusmurphy.chesses;
+
+import fun.gusmurphy.chesses.engine.Coordinates;
+
+import java.util.Arrays;
+
+public class CoordinatesXyAdapter {
+
+    private final int x;
+    private final int y;
+    private final Coordinates coordinates;
+
+    public CoordinatesXyAdapter(Coordinates coordinates) {
+        this.x = coordinates.file().ordinal();
+        this.y = coordinates.rank().ordinal();
+        this.coordinates = coordinates;
+    }
+
+    public CoordinatesXyAdapter(int x, int y) {
+        this.x = x;
+        this.y = y;
+
+        coordinates = Arrays.stream(Coordinates.values())
+            .filter(c -> c.file().ordinal() == x)
+            .filter(c -> c.rank().ordinal() == y)
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("Couldn't derive board coordinates from (" + x + ", " + y + ")"));
+    }
+
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
+
+    public Coordinates coordinates() {
+        return coordinates;
+    }
+
+}
