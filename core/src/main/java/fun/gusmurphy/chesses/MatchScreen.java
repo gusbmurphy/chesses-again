2ffc7e7ea56f78094af08e7a8778e3aa25bcc82e
Augustus Murphy <gusbmurphy@gmail.com>
2025-08-19 22:14:48 -0500

    Make `MatchScreen` the `PieceSelectionListener`

--- core/src/main/java/fun/gusmurphy/chesses/MatchScreen.java
+++ core/src/main/java/fun/gusmurphy/chesses/MatchScreen.java
@@ -8,13 +8,18 @@ import fun.gusmurphy.chesses.engine.boardstate.BoardCoordinateStates;
 import fun.gusmurphy.chesses.engine.boardstate.BoardState;
 import fun.gusmurphy.chesses.engine.boardstate.BoardStateBuilder;
 import fun.gusmurphy.chesses.engine.piece.Piece;
+import fun.gusmurphy.chesses.engine.piece.PieceId;
 import fun.gusmurphy.chesses.engine.piece.PieceType;
 import fun.gusmurphy.chesses.piece.PieceDrawable;
+import fun.gusmurphy.chesses.piece.PieceOnScreenSelectionListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MatchScreen extends BaseScreen {
+public class MatchScreen extends BaseScreen implements PieceOnScreenSelectionListener {
+
+    private PieceId selectedPieceId;
+    private List<PieceDrawable> pieceDrawables;
 
     public MatchScreen(final ChessesGame game) {
         super(game);
@@ -37,8 +42,37 @@ public class MatchScreen extends BaseScreen {
             });
         }
 
+        this.pieceDrawables = pieceDrawables;
         drawables.add(board);
         drawables.addAll(pieceDrawables);
+    }
+
+    @Override
+    public void onPieceSelected(PieceId pieceId) {
+        if (selectedPieceId == null) {
+            updateSelectedPiece(pieceId);
+        }
+    }
+
+    @Override
+    public void onPieceReleased(PieceId pieceId, Vector2 screenPosition) {
+        if (selectedPieceId == pieceId) {
+            unselectPiece(pieceId);
+        }
+    }
+
+    private void updateSelectedPiece(PieceId pieceId) {
+        selectedPieceId = pieceId;
+        PieceDrawable pieceDrawable = pieceDrawables
+            .stream().filter(piece -> piece.pieceId() == pieceId).findFirst().get();
+        pieceDrawable.setDragStatus(true);
+    }
+
+    private void unselectPiece(PieceId pieceId) {
+        selectedPieceId = null;
+        PieceDrawable pieceDrawable = pieceDrawables
+            .stream().filter(piece -> piece.pieceId() == pieceId).findFirst().get();
+        pieceDrawable.setDragStatus(false);
     }
 
 }
