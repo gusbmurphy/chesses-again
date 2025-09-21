40bf4a490d9dc184a273a46b283a5931e5278374
Augustus Murphy <gusbmurphy@gmail.com>
2025-09-21 16:37:54 -0500

    Add engine to `MatchScreen`

--- core/src/main/java/fun/gusmurphy/chesses/screen/MatchScreen.java
+++ core/src/main/java/fun/gusmurphy/chesses/screen/MatchScreen.java
@@ -3,14 +3,10 @@ package fun.gusmurphy.chesses.screen;
 import com.badlogic.gdx.math.Vector2;
 import fun.gusmurphy.chesses.ChessesGame;
 import fun.gusmurphy.chesses.board.BoardDrawable;
-import fun.gusmurphy.chesses.engine.Coordinates;
-import fun.gusmurphy.chesses.engine.PlayerColor;
+import fun.gusmurphy.chesses.engine.ChessEngine;
 import fun.gusmurphy.chesses.engine.boardstate.BoardCoordinateState;
-import fun.gusmurphy.chesses.engine.boardstate.BoardState;
-import fun.gusmurphy.chesses.engine.boardstate.BoardStateBuilder;
 import fun.gusmurphy.chesses.engine.piece.Piece;
 import fun.gusmurphy.chesses.engine.piece.PieceId;
-import fun.gusmurphy.chesses.engine.piece.PieceType;
 import fun.gusmurphy.chesses.piece.PieceDrawable;
 import fun.gusmurphy.chesses.piece.PieceSelectionListener;
 
@@ -21,9 +17,11 @@ public class MatchScreen extends BaseScreen implements PieceSelectionListener {
 
     private PieceId selectedPieceId;
     private List<PieceDrawable> pieceDrawables;
+    private final ChessEngine engine;
 
     public MatchScreen(final ChessesGame game) {
         super(game);
+        engine = ChessEngine.defaultEngine();
         BoardDrawable board = setupBoard();
         setupPieceDrawables(board);
     }
@@ -43,13 +41,7 @@ public class MatchScreen extends BaseScreen implements PieceSelectionListener {
     }
 
     private BoardDrawable setupBoard() {
-        BoardState initialBoardState = new BoardStateBuilder()
-            .addPieceAt(new Piece(PlayerColor.BLACK, PieceType.ROOK), Coordinates.A1)
-            .addPieceAt(new Piece(PlayerColor.WHITE, PieceType.BISHOP), Coordinates.C3)
-            .height(3)
-            .width(7)
-            .build();
-        BoardDrawable board = new BoardDrawable(game, initialBoardState);
+        BoardDrawable board = new BoardDrawable(game, engine.currentBoardState());
         drawables.add(board);
         return board;
     }
