20011fd932c43a57013c5a3bf0d37dcacb17f7cd
Augustus Murphy <gusbmurphy@gmail.com>
2025-09-21 16:13:23 -0500

    Add `defaultBoard` to `BoardStateBuilder`

--- engine/src/main/java/fun/gusmurphy/chesses/engine/boardstate/BoardStateBuilder.java
+++ engine/src/main/java/fun/gusmurphy/chesses/engine/boardstate/BoardStateBuilder.java
@@ -5,11 +5,14 @@ import fun.gusmurphy.chesses.engine.File;
 import fun.gusmurphy.chesses.engine.Rank;
 import fun.gusmurphy.chesses.engine.piece.Piece;
 import fun.gusmurphy.chesses.engine.PlayerColor;
+import fun.gusmurphy.chesses.engine.piece.PieceType;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static fun.gusmurphy.chesses.engine.piece.PieceType.*;
+import static fun.gusmurphy.chesses.engine.PlayerColor.*;
 
 public class BoardStateBuilder {
 
@@ -59,6 +62,45 @@ public class BoardStateBuilder {
         if (width * height < 2) {
             throw new IllegalArgumentException("Board must have at least two coordinates");
         }
+    }
+
+    public static BoardState defaultBoard() {
+        BoardStateBuilder builder = new BoardStateBuilder()
+            .currentTurnColor(WHITE);
+
+        Map<File, PieceType> backRankPieceTypesByFile = Stream.of(
+            new AbstractMap.SimpleEntry<>(File.A, ROOK),
+            new AbstractMap.SimpleEntry<>(File.B, KNIGHT),
+            new AbstractMap.SimpleEntry<>(File.C, BISHOP),
+            new AbstractMap.SimpleEntry<>(File.D, KING),
+            new AbstractMap.SimpleEntry<>(File.E, QUEEN),
+            new AbstractMap.SimpleEntry<>(File.F, BISHOP),
+            new AbstractMap.SimpleEntry<>(File.G, KNIGHT),
+            new AbstractMap.SimpleEntry<>(File.H, ROOK)
+        ).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        Rank whitePawnRank = Rank.TWO;
+        Rank whiteBackRank = Rank.ONE;
+        Rank blackPawnRank = Rank.SEVEN;
+        Rank blackBackRank = Rank.EIGHT;
+
+        for (File file : File.values()) {
+            PieceType backRankPieceType = backRankPieceTypesByFile.get(file);
+
+            Piece whiteBackRankPiece = new Piece(WHITE, backRankPieceType);
+            Piece blackBackRankPiece = new Piece(BLACK, backRankPieceType);
+
+            builder.addPieceAt(whiteBackRankPiece, Coordinates.with(file, whiteBackRank));
+            builder.addPieceAt(blackBackRankPiece, Coordinates.with(file, blackBackRank));
+
+            Piece whitePawn = new Piece(WHITE, PAWN);
+            Piece blackPawn = new Piece(BLACK, PAWN);
+
+            builder.addPieceAt(whitePawn, Coordinates.with(file, whitePawnRank));
+            builder.addPieceAt(blackPawn, Coordinates.with(file, blackPawnRank));
+        }
+
+        return builder.build();
     }
 
 }
