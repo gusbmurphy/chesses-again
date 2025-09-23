9322aea7a1fa56451a0115fa9aa83ec243b71e38
Augustus Murphy <gusbmurphy@gmail.com>
2025-09-22 21:37:54 -0500

    Ignore non-knights in `KnightMovementRule`

--- engine/src/main/java/fun/gusmurphy/chesses/engine/rules/KnightMovementRule.java
+++ engine/src/main/java/fun/gusmurphy/chesses/engine/rules/KnightMovementRule.java
@@ -2,10 +2,18 @@ package fun.gusmurphy.chesses.engine.rules;
 
 import fun.gusmurphy.chesses.engine.Move;
 import fun.gusmurphy.chesses.engine.boardstate.BoardState;
+import fun.gusmurphy.chesses.engine.piece.PieceOnBoard;
+import fun.gusmurphy.chesses.engine.piece.PieceType;
 
 public class KnightMovementRule implements MoveLegalityRule {
     @Override
     public MoveLegality evaluate(BoardState boardState, Move move) {
+        PieceOnBoard pieceOnBoard = boardState.pieceOnBoardForId(move.pieceId());
+
+        if (pieceOnBoard.type() != PieceType.KNIGHT) {
+            return MoveLegality.UNCONCERNED;
+        }
+
         return MoveLegality.LEGAL;
     }
 }
--- engine/src/test/groovy/fun/gusmurphy/chesses/engine/rules/KnightMovementRuleSpec.groovy
+++ engine/src/test/groovy/fun/gusmurphy/chesses/engine/rules/KnightMovementRuleSpec.groovy
@@ -8,14 +8,32 @@ import fun.gusmurphy.chesses.engine.piece.PieceType
 import spock.lang.Specification
 
 import static fun.gusmurphy.chesses.engine.Coordinates.*
+import static fun.gusmurphy.chesses.engine.PlayerColor.*
+import static fun.gusmurphy.chesses.engine.piece.PieceType.*
 
 class KnightMovementRuleSpec extends Specification {
 
     private static final MoveLegalityRule KNIGHT_RULE = new KnightMovementRule()
 
+    def "the movement rule is not concerned with a non-knight piece"() {
+        given:
+        def nonKnight = new Piece(WHITE, pieceType as PieceType)
+        def board = new BoardStateBuilder().addPieceAt(nonKnight, D4).build()
+        def move = new Move(nonKnight.id(), C2)
+
+        when:
+        def result = KNIGHT_RULE.evaluate(board, move)
+
+        then:
+        result == MoveLegality.UNCONCERNED
+
+        where:
+        pieceType << [ROOK, BISHOP, KING, QUEEN, PAWN]
+    }
+
     def "the knight can move in an L shape"() {
         given:
-        def piece = new Piece(PlayerColor.BLACK, PieceType.KNIGHT)
+        def piece = new Piece(BLACK, KNIGHT)
         def board = new BoardStateBuilder().addPieceAt(piece, D5).build()
 
         expect:
