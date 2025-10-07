5658a846a44296bbd1b397718de804e248ed77ce
Augustus Murphy <gusbmurphy@gmail.com>
2025-10-06 22:31:08 -0500

    Add test that only pawns are relevant

--- engine/src/test/groovy/fun/gusmurphy/chesses/engine/rules/PawnTakingRuleSpec.groovy
+++ engine/src/test/groovy/fun/gusmurphy/chesses/engine/rules/PawnTakingRuleSpec.groovy
@@ -2,14 +2,20 @@ package fun.gusmurphy.chesses.engine.rules
 
 import spock.lang.Specification
 
+import static fun.gusmurphy.chesses.engine.piece.PieceType.*
+
 class PawnTakingRuleSpec extends Specification {
 
-    def "the rule overrides the pawn movement rule"() {
-        given:
-        MoveRule rule = new PawnTakingRule()
+    static rule = new PawnTakingRule()
 
+    def "the rule overrides the pawn movement rule"() {
         expect:
         rule.overrides(new PawnMovementRule())
+    }
+
+    def "the rule is relevant for pawns"() {
+        expect:
+        rule.isRelevantForPieceType(PAWN)
     }
 
 }
