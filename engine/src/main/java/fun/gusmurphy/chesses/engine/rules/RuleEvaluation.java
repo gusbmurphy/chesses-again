ad8a33ba0c9376f7a8843dfb85204ff4882a25e4
Augustus Murphy <gusbmurphy@gmail.com>
2025-10-28 09:49:11 -0400

    Convert `RuleEvaluation` to a class

--- engine/src/main/java/fun/gusmurphy/chesses/engine/rules/RuleEvaluation.java
+++ engine/src/main/java/fun/gusmurphy/chesses/engine/rules/RuleEvaluation.java
@@ -1,13 +1,42 @@
 package fun.gusmurphy.chesses.engine.rules;
 
-public enum RuleEvaluation {
-    LEGAL, ILLEGAL, UNCONCERNED;
+public class RuleEvaluation {
+    private final Legality legality;
+    private final Effects effects;
+
+    public final static RuleEvaluation ILLEGAL = new RuleEvaluation(
+        Legality.ILLEGAL,
+        Effects.emptyEffects()
+    );
+
+    public final static RuleEvaluation LEGAL = new RuleEvaluation(
+        Legality.LEGAL,
+        Effects.emptyEffects()
+    );
+
+    public final static RuleEvaluation UNCONCERNED = new RuleEvaluation(
+        Legality.UNCONCERNED,
+        Effects.emptyEffects()
+    );
+
+    private RuleEvaluation(Legality legality, Effects effects) {
+        this.legality = legality;
+        this.effects = effects;
+    }
 
     public Effects effects() {
-        return new Effects();
+        return effects;
+    }
+
+    public enum Legality {
+        LEGAL, ILLEGAL, UNCONCERNED;
     }
 
     public static class Effects {
+        protected static Effects emptyEffects() {
+            return new Effects();
+        }
+
         public boolean areNone() {
             return true;
         }
