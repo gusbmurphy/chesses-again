d81eb1ef211af832952609ad6228930924ec6adb
Augustus Murphy <gusbmurphy@gmail.com>
2025-10-29 17:32:40 -0400

    Expand castling test

--- engine/src/test/groovy/fun/gusmurphy/chesses/engine/rules/CastlingRuleSpec.groovy
+++ engine/src/test/groovy/fun/gusmurphy/chesses/engine/rules/CastlingRuleSpec.groovy
@@ -11,20 +11,27 @@ import static fun.gusmurphy.chesses.engine.rules.RuleEvaluation.Legality.*
 
 class CastlingRuleSpec extends MoveRuleSpecification {
 
-    private static final MoveRule CASTLING_RULE = new CastlingRule()
+    private static MoveRule rule = new CastlingRule()
 
     def "castling moves the king and rook in one move"() {
         given:
-        def king = new Piece(WHITE, KING)
-        def rook = new Piece(WHITE, ROOK)
+        def king = new Piece(color, KING)
+        def rook = new Piece(color, ROOK)
         def board = new BoardStateBuilder()
-            .addPieceAt(king, E1)
-            .addPieceAt(rook, A1)
+            .addPieceAt(king, kingPosition)
+            .addPieceAt(rook, rookPosition)
             .build()
-        def move = new Move(king.id(), E1)
+        def move = new Move(king.id(), moveCoordinates)
 
         expect:
-        CASTLING_RULE.evaluate(board, move).legality() == LEGAL
+        rule.evaluate(board, move).legality() == LEGAL
+
+        where:
+        color | kingPosition | rookPosition | moveCoordinates
+        WHITE | E1           | A1           | C1
+        WHITE | E1           | H1           | G1
+        BLACK | E8           | A8           | C8
+        BLACK | E8           | H8           | G8
     }
 
 }
