738cc1355d18598c8a5a99b1e932642c5727b27a
Augustus Murphy <gusbmurphy@gmail.com>
2025-11-23 17:13:25 -0600

    Delete `MoveEventDeriver`

--- engine/src/main/java/fun/gusmurphy/chesses/engine/DerivesMoveEvents.java
+++
@@ -1,9 +0,0 @@
-package fun.gusmurphy.chesses.engine;
-
-import fun.gusmurphy.chesses.engine.boardstate.BoardState;
-import fun.gusmurphy.chesses.engine.events.BoardStateEvents;
-
-public interface DerivesMoveEvents {
-
-    BoardStateEvents deriveEventsFrom(Move move, BoardState boardState);
-}
